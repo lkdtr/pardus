7a53372d097a7d8d3d4f1d6b97d0f345dad00c2a
doruk.fisek <doruk.fisek@8357347a-4f75-4f86-90e0-a17bfb4142b2>
2010-01-19 11:23:06 +0000

    merge: desktop.lookandfeel + moodinkde/fingerprint ksplash themes

---
+++ corporate2/stable/desktop/lookandfeel/moodin-ksplash-themes/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+WorkDir='moodin'
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+    # Avoid file-conflicts with kdebase
+    pisitools.remove("/usr/kde/3.5/lib/kde3/ksplashmoodin.so")
+    pisitools.remove("/usr/kde/3.5/share/services/ksplashmoodin.desktop")
+
+    pisitools.dodoc("COPYING", "AUTHORS")   
