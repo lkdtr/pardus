8a2352d5f40639820f0511121f5cd973f1257f77
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2010-06-15 10:27:18 +0000

    Version bump to new beta for testing

--- corporate2/devel/network/mail/sylpheed/actions.py
+++ corporate2/devel/network/mail/sylpheed/actions.py
@@ -9,6 +9,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir='sylpheed-3.1.0beta1' 
+
 docdir = '/%s/%s' % (get.docDIR(), get.srcNAME())
 
 def setup():
