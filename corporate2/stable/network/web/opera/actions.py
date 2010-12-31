4ede776f2ed7596df173ce6822de3ff11fe24a5b
doruk.fisek <doruk.fisek@8357347a-4f75-4f86-90e0-a17bfb4142b2>
2010-12-31 11:56:42 +0000

    derleme ciftligi yolculari

--- corporate2/stable/network/web/opera/actions.py
+++ corporate2/stable/network/web/opera/actions.py
@@ -5,17 +5,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="%s-%s-6450.i386.linux" % (get.srcNAME(), get.srcVERSION())
+WorkDir="%s-%s-1156.i386.linux" % (get.srcNAME(), get.srcVERSION())
+
+def build():
+    # Flashplugin hack for Opera, see pardus #13989
+    autotools.compile("-shared -fPIC -L/opt/netscape/plugins/ -lflashplayer \
+                       -o libflashplayer.so -Wl,-rpath \
+                       /opt/netscape/plugins/ opera-flash-workaround.c")
 
 def install():
-    pisitools.dosed("install", "dest=\$PREFIX", "dest=%s/usr" % get.installDIR())
-    pisitools.dosed("install", "\$HOME/.local", "/usr")
-    pisitools.dosed("install", "update-[a-z]*-database", "XXpatchedXX")
+    shelltools.system("./install --prefix /usr --repackage %s/usr" % get.installDIR())
 
-    shelltools.system("./install --unattended --user")
-
-    pisitools.dosym("/opt/netscape/plugins/libflashplayer.so", "/usr/lib/opera/plugins/libflashplayer.so")
+    pisitools.insinto("/usr/lib/opera/plugins/", "libflashplayer.so")
