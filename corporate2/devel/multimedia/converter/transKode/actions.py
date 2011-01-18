a432fe709b5e79908b13f724fde5c86975919c76
doruk.fisek <doruk.fisek@8357347a-4f75-4f86-90e0-a17bfb4142b2>
2011-01-18 23:21:11 +0000

    transKode -- ses dosyalarinin cevirimi icin KDE uygulamasi

---
+++ corporate2/devel/multimedia/converter/transKode/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = 'transkode'
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+    pisitools.dodir("/usr/kde/3.5/share/applnk/Multimedia")
+    pisitools.domove("/usr/kde/3.5/share/applnk/transkode.desktop", "/usr/kde/3.5/share/applnk/Multimedia")
