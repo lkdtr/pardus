9cbd4ab6a6376b5df78a0b4a3158e0d359ca589d
doruk.fisek <doruk.fisek@8357347a-4f75-4f86-90e0-a17bfb4142b2>
2011-02-26 22:52:40 +0000

    Derleme ciftligine 4 yeni paket

---
+++ corporate2/stable/game/puzzle/hangman/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.dosed("src/hangman.c", "inline void SE_CheckEvents", "void SE_CheckEvents")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
