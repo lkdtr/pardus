2e3f62b27f8870d215478d7359e2c303ca2577b9
samed.beyribey <samed.beyribey@8357347a-4f75-4f86-90e0-a17bfb4142b2>
2011-12-26 07:50:00 +0000

    nagstamon paketini ekledim.

---
+++ playground/samed.beyribey/server/monitor/nagstamon/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "Nagstamon"
+
+def install():
+    pythonmodules.install()
+    pisitools.dosym("/usr/bin/nagstamon.py", "/usr/bin/nagstamon")
